4977013769ca8358bf854f8ba33d5b020204ac38
Raymond van der Gugten <Montoo94@gmail.com>
2015-05-25 15:30:12 +0200

    Test

--- src/doolhof/Speler.java
+++ src/doolhof/Speler.java
@@ -13,6 +13,8 @@ import javax.swing.JLabel;
 public class Speler {
 
     private Tegel lokatie;
+    private Valsspeler vsp;
+    private Speler speler;
     JLabel score=new JLabel();
     public Speler(JLabel score) {
         this.score=score;
@@ -28,31 +30,68 @@ public class Speler {
 
     public void move(String richting) {
         int som;
-        if (richting.equals("w") && lokatie.getNorth().getIsGone()) {
+        if (richting.equals("w") && lokatie.getNorth().getIsGone() && lokatie.getNorthBuur().getValsspeler() == null) {
             lokatie.setSpeler(null);
             lokatie.getNorthBuur().setSpeler(this);
             lokatie=lokatie.getNorthBuur();
             som=Integer.parseInt(score.getText())+1;
             score.setText(""+som);
         }
-        if (richting.equals("a") && lokatie.getWest().getIsGone()) {
+        if (richting.equals("a") && lokatie.getWest().getIsGone() && lokatie.getWestBuur().getValsspeler() == null) {
             lokatie.setSpeler(null);
             lokatie.getWestBuur().setSpeler(this);
             lokatie=lokatie.getWestBuur();
             som=Integer.parseInt(score.getText())+1;
             score.setText(""+som);
         }
-        if (richting.equals("s") && lokatie.getSouthBuur().getNorth().getIsGone()) {
+        if (richting.equals("s") && lokatie.getSouthBuur().getNorth().getIsGone() && lokatie.getSouthBuur().getValsspeler() == null) {
             lokatie.setSpeler(null);
             lokatie.getSouthBuur().setSpeler(this);
             lokatie=lokatie.getSouthBuur();
             som=Integer.parseInt(score.getText())+1;
             score.setText(""+som);
         }
-        if (richting.equals("d") && lokatie.getEastBuur().getWest().getIsGone()) {
+        if (richting.equals("d") && lokatie.getEastBuur().getWest().getIsGone() && lokatie.getEastBuur().getValsspeler() == null) {
             lokatie.setSpeler(null);
             lokatie.getEastBuur().setSpeler(this);
             lokatie=lokatie.getEastBuur();
+            
+            som=Integer.parseInt(score.getText())+1;
+            score.setText(""+som);
+        }
+        //Hieronder Valsspeler collision
+        
+        if (richting.equals("w") && lokatie.getNorth().getIsGone() && lokatie.getNorthBuur().getValsspeler() != null) {
+            lokatie.setSpeler(null);
+            vsp.zetSpelerTerug().setSpeler(speler);
+            speler.setLokatie(vsp.zetSpelerTerug());
+            lokatie.setSpeler(speler);
+            
+            
+            som=Integer.parseInt(score.getText())+1;
+            score.setText(""+som);
+        }
+        if (richting.equals("a") && lokatie.getWest().getIsGone() && lokatie.getWestBuur().getValsspeler() != null) {
+            lokatie.setSpeler(null);
+            vsp.zetSpelerTerug().setSpeler(speler);
+            speler.setLokatie(vsp.zetSpelerTerug());
+            lokatie.setSpeler(speler);
+            som=Integer.parseInt(score.getText())+1;
+            score.setText(""+som);
+        }
+        if (richting.equals("s") && lokatie.getSouthBuur().getNorth().getIsGone() && lokatie.getSouthBuur().getValsspeler() != null) {
+            lokatie.setSpeler(null);
+            vsp.zetSpelerTerug().setSpeler(speler);
+            speler.setLokatie(vsp.zetSpelerTerug());
+            lokatie.setSpeler(speler);
+            som=Integer.parseInt(score.getText())+1;
+            score.setText(""+som);
+        }
+        if (richting.equals("d") && lokatie.getEastBuur().getWest().getIsGone() && lokatie.getEastBuur().getValsspeler() != null) {
+            lokatie.setSpeler(null);
+            vsp.zetSpelerTerug().setSpeler(speler);
+            speler.setLokatie(vsp.zetSpelerTerug());
+            lokatie.setSpeler(speler);
             
             som=Integer.parseInt(score.getText())+1;
             score.setText(""+som);
--- src/doolhof/Valsspeler.java
+++ src/doolhof/Valsspeler.java
@@ -5,18 +5,43 @@
  */
 package doolhof;
 
+import java.util.Random;
+
 /**
  *
  * @author Montoo
  */
 public class Valsspeler extends Persoon {
     
+        private Tegel locatieSpeler;
+        private Tegel[][] tegel;
+        private Speler speler;
+    
         public Tegel getLokatie() {
+            
         return locatie;
     }
 
     public void setLokatie(Tegel Lokatie) {
         this.locatie = Lokatie;
+    }
+    
+    public Tegel zetSpelerTerug() {
+        
+        int oudeLocatieX = locatieSpeler.getSpeler().getLokatie().getX();
+        int oudeLocatieY = locatieSpeler.getSpeler().getLokatie().getY();
+        
+        Random r = new Random();
+        int randomX = r.nextInt(5);
+        int randomY = r.nextInt(5);
+        
+        int nieuweLocatieX = oudeLocatieX - 5;
+        int nieuweLocatieY = oudeLocatieY - 5;
+        
+        locatieSpeler = tegel[nieuweLocatieX][nieuweLocatieY];
+        
+        return locatieSpeler;
+        
     }
     
 }
